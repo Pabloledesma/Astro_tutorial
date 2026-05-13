594d90509e12d2b33198f128175080838bbace06
Pabloledesma <pabloledes83@gmail.com>
2026-05-12 20:08:07 -0500

    install rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://silly-beignet-10d67b.netlify.app/"
+});
